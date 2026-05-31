553d3f7df40dca4d32c43e92afa6e283ee16ca3c
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2026-05-31 07:31:21 +0300

    Prevent test task from failing if no tests are discovered

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,9 @@ plugins {
     alias(libs.plugins.pluginYml) apply false
     alias(libs.plugins.buildconfig) apply false
 }
+
+allprojects {
+    tasks.withType<Test> {
+        failOnNoDiscoveredTests = false
+    }
+}
