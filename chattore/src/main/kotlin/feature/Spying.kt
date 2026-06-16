c8d5d1dfc9fe21b78c3b612f419739fbb3d4fe2e
Pauli Kauro <3965357+paulikauro@users.noreply.github.com>
2026-06-16 11:46:08 +0300

    Separate spying into command and social spy

--- chattore/src/main/kotlin/feature/Spying.kt
+++ chattore/src/main/kotlin/feature/Spying.kt
@@ -3,7 +3,6 @@ package org.openredstone.chattore.feature
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
-import co.aikar.commands.annotation.Default
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.command.CommandExecuteEvent
 import com.velocitypowered.api.proxy.Player
@@ -12,21 +11,23 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.space
 import org.openredstone.chattore.*
 
-private val SpyEnabled = Setting<Boolean>("spy")
+// TODO: rename the key, requires a DB migration
+private val CommandSpyEnabled = Setting<Boolean>("spy")
+private val SocialSpyEnabled = Setting<Boolean>("socialSpy")
 
 typealias Wiretap = (Component) -> Unit
 
-fun PluginScope.createSpyingFeature(
-    database: Storage,
-    formatConfig: FormatConfig,
-): Wiretap {
-    // TODO: this hits the DB way WAY way too often xd
-    fun Player.isSpying() = database.getSetting(SpyEnabled, uniqueId) ?: false
-    val spies = proxy.all { it.hasChattorePrivilege && it.isSpying() }
-    registerCommands(CommandSpy(database))
-    registerListeners(CommandListener(spies))
+fun PluginScope.createSpyingFeature(database: Storage, formatConfig: FormatConfig): Wiretap {
+    fun spyAudience(spySetting: Setting<Boolean>) =
+        proxy.all { it.hasChattorePrivilege && database.getSetting(spySetting, it.uniqueId) == true }
+
+    val commandSpies = spyAudience(CommandSpyEnabled)
+    val socialSpies = spyAudience(SocialSpyEnabled)
+    registerCommands(SpyCommands(database))
+    registerListeners(CommandListener(commandSpies))
+
     val spyPrefix = formatConfig.spyPrefix.render().append(space())
-    return { secrets -> spies.sendMessage(spyPrefix.append(secrets)) }
+    return { secrets -> socialSpies.sendMessage(spyPrefix.append(secrets)) }
 }
 
 private class CommandListener(
@@ -42,21 +43,29 @@ private class CommandListener(
     }
 }
 
-@CommandAlias("commandspy")
-@CommandPermission("chattore.commandspy")
-private class CommandSpy(
+private class SpyCommands(
     private val database: Storage,
 ) : BaseCommand() {
-    @Default
-    fun default(player: Player) {
-        val setting = database.getSetting(SpyEnabled, player.uniqueId)
-        val newSetting = !(setting ?: false)
-        database.setSetting(SpyEnabled, player.uniqueId, newSetting)
+    @CommandAlias("commandspy")
+    @CommandPermission("chattore.commandspy")
+    fun commandSpy(player: Player) {
+        toggleSpy(player, CommandSpyEnabled, "Command spy")
+    }
+
+    @CommandAlias("socialspy")
+    @CommandPermission("chattore.socialspy")
+    fun socialSpy(player: Player) {
+        toggleSpy(player, SocialSpyEnabled, "Social spy")
+    }
+
+    private fun toggleSpy(player: Player, spySetting: Setting<Boolean>, kind: String) {
+        val newSetting = database.getSetting(spySetting, player.uniqueId) != true
+        database.setSetting(spySetting, player.uniqueId, newSetting)
         player.sendInfo(
             if (newSetting) {
-                "You are now spying on commands."
+                "$kind enabled."
             } else {
-                "You are no longer spying on commands."
+                "$kind disabled."
             },
         )
     }
